d6ed9e4daaf7833b3049b168f866ef7bd0b76fc2
Samriddhi <55628135+Samriddhi5@users.noreply.github.com>
2025-03-29 20:44:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function showMessage() {
     alert("Hello! Welcome to my website.");
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
